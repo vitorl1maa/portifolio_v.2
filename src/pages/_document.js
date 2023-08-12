a08f0d7580f8c928a0305054b2cbb3bd4f966749
vitorl1maa <vitornascimento923@gmail.com>
2023-08-11 23:37:00 -0300

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Portifólio</title>
+      <link rel="icon" href="/logo.png" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
